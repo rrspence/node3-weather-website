1a73afc9514f71df98c3844fb769c46911048679
rrspence <spencer.rahim@gmail.com>
2020-08-06 21:28:49 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=055e09b206bfa790c1b19e34f1dde598&query=' + latitude + ',' + longitude + '&units=f';
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service.', undefined);
+        } else if (body.error) {
+            callback('Unabled to find location.', undefined);
+        } else {
+            callback(
+              undefined,
+              body.current.weather_descriptions[0] +
+                ". It is currently " +
+                body.current.temperature +
+                " degrees out. It feels like " +
+                body.current.feelslike +
+                " degrees out."
+            );
+        }
+    });
+};
+
+module.exports = forecast;
