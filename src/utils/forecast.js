97ddd2d61f43cefb1f47c07fe03da061c7147fe1
rrspence <spencer.rahim@gmail.com>
2020-08-07 18:08:11 -0600

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (latitude, longitude, callback) => {
                 body.current.temperature +
                 " degrees out. It feels like " +
                 body.current.feelslike +
-                " degrees out."
+                " degrees out. The humidity is " + body.current.humidity + "%."
             );
         }
     });
